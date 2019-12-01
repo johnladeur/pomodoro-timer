b1372c5dced4cabb023a1dffa5ab005df29e866e
John LaDeur <jdladeur@gmail.com>
2019-11-30 21:46:00 -0600

    added HTML layout, declared button variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const breakBtn = document.getElementById('break-btn');
+const workBtn = document.getElementById('work-btn');
+const startBtn = document.getElementById('start-btn');
+const stopBtn = document.getElementById('stop-btn');
+const resetBtn = document.getElementById('reset-btn');
+
