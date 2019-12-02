691b90b0aa182e477a48219a2f38fbded71e039f
John LaDeur <jdladeur@gmail.com>
2019-12-01 20:53:53 -0600

    Added Reset button functionality and set condition for timer to stop at 0:00

--- script.js
+++ script.js
@@ -1,41 +1,50 @@
-let timer = setInterval(timerHandler, 1000);
-let sec = 0;
-let min = 25;
-
-
-let startButton = document.getElementById('start-btn')
-
-startButton.addEventListener('click', function(){
-    timer = setInterval(timerHandler, 1000)
-})
-
-let stopButton = document.getElementById('stop-btn')
-    stopButton.addEventListener('click', function(){
-        timer = clearInterval(timer)
-        console.log('stop button clicked')
-    })
-
-function timerHandler(){
- if (sec >= 1) {
-    sec--
-} else {
-    sec = 59
-    min--
-   
-} if (sec < 10){
-    sec = "0" + sec
-} if (min && sec < 2){
-    min = 25
-    sec = 0
-}
-displayTime()
-}
-
-function displayTime(){
-    let timer = document.getElementById('time-remaining')
-    timer.innerHTML = min + ":" + sec;
+(function() {
+  let sec = 4;
+  let min = 0;
+  let startButton = document.getElementById("start-btn");
+  let stopButton = document.getElementById("stop-btn");
+  let resetButton = document.getElementById("reset-btn");
+  
+  startButton.addEventListener("click", function() {
+    timer = setInterval(timerHandler, 1000);
+    resetButton.disabled = true;
+  });
+  
+  stopButton.addEventListener("click", function() {
+    timer = clearInterval(timer);
+    console.log("stop button clicked");
+    resetButton.disabled = false;
+  });
+  
+  resetButton.addEventListener("click", function() {
+    console.log('reset button clicked')
+    min = 25;
+    sec = 1;
     
-}
-
-
-
+    resetButton.disabled = true;
+  });
+  
+  function timerHandler() {
+     
+    if (sec >= 1) {
+      sec--;
+    } else {
+      sec = 59;
+      min--;
+    }
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    if (min == 0 && sec == 0) {
+      timer = clearInterval(timer);
+      resetButton.disabled = false;
+    }
+    displayTime();
+  }
+  
+  function displayTime() {
+    let timer = document.getElementById("time-remaining");
+    timer.innerHTML = min + ":" + sec;
+  }
+  })();
+  
