f2b7229b3742e1378e0caa2473b520c8b33056c7
John LaDeur <jdladeur@gmail.com>
2019-12-01 18:44:06 -0600

    added functionality to start and stop buttons

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-const breakBtn = document.getElementById('break-btn');
-const workBtn = document.getElementById('work-btn');
-const startBtn = document.getElementById('start-btn');
-const stopBtn = document.getElementById('stop-btn');
-const resetBtn = document.getElementById('reset-btn');
+let timer = setInterval(timerHandler, 1000);
+let sec = 0;
+let min = 25;
+
+
+let startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', function(){
+    timer = setInterval(timerHandler, 1000)
+})
+
+let stopButton = document.getElementById('stop-btn')
+    stopButton.addEventListener('click', function(){
+        timer = clearInterval(timer)
+        console.log('stop button clicked')
+    })
+
+function timerHandler(){
+ if (sec >= 1) {
+    sec--
+} else {
+    sec = 59
+    min--
+   
+} if (sec < 10){
+    sec = "0" + sec
+} if (min && sec < 2){
+    min = 25
+    sec = 0
+}
+displayTime()
+}
+
+function displayTime(){
+    let timer = document.getElementById('time-remaining')
+    timer.innerHTML = min + ":" + sec;
+    
+}
+
+
 
